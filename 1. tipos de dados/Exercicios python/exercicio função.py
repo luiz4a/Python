b4d990ed3f9a62a6991c95df48f5271422d38766
luiz4a <159453767+luiz4a@users.noreply.github.com>
2024-05-23 14:03:22 +0000

    HGHGFHG

--- 1. tipos de dados/Exercicios python/exercicio função.py
+++ 1. tipos de dados/Exercicios python/exercicio função.py
@@ -1,21 +1,37 @@
 import os
 
 
-
 def cabeçalho():
     os.system("cls||clear")
     print("====MARIERS'CODE====")
 
+def verificar_impares (numeros):
+    impares = 0
+    for numero in numeros :
+     if numero % 2 != 0:
+       impares += 1
+    return impares
 
-def par_ou_impar (numero):
-    if numero % 2 == 0:
-       return "par"
-    else:
-       return "impar"
-
-cabeçalho()    
-numero = int(input("Digite um numero: "))   
+def verificar_pares (numeros):
+    pares = 0
+    for numero in numeros: 
+     if numero % 2 == 0:
+       pares += 1
+    return pares
+     
+QTD = 6
+lista_numeros = []
 
 cabeçalho()
-resultado = par_ou_impar (numero)
-print(f"O número {numero} é {resultado}.")
+for i in range(QTD):
+    numero = int(input("Digite um numero: "))   
+    lista_numeros.append(numero)
+     
+
+quantidade_pares = verificar_pares (lista_numeros)
+quantidade_impares = verificar_impares (lista_numeros)
+
+
+cabeçalho()
+print(f"Quantidade de numeros impares: {quantidade_impares}")
+print(f"Quantidade de pares é: {quantidade_pares}")
